88eb1e8e97c9b0249d23fa5a44d7bd59604549b8
Erik Baliov <vfxbaliof@gmail.com>
2018-03-21 17:38:47 +0200

    Documentation on solution 02

--- Solution 02/problem02.cs
+++ Solution 02/problem02.cs
@@ -6,11 +6,14 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
+			// Getting the input from the console
            var a = double.Parse(Console.ReadLine());
            var b = double.Parse(Console.ReadLine());
 
+		   // Doing calculations
            var S = a * b;
 
+		   // Printing the result
             Console.WriteLine($"{S:f2}");
 
         }
