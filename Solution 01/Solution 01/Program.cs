ba0b5e2d95b438485aa9a85dd86bc211caa1a814
mariaslytherin <nirvana4ever>
2018-03-21 17:48:31 +0200

    changed types of variables

--- Solution 01/Solution 01/Program.cs
+++ Solution 01/Solution 01/Program.cs
@@ -7,10 +7,10 @@ namespace Solution_01
         static void Main(string[] args)
         {   
             //initializing four integers 
-            int firstNumber = int.Parse(Console.ReadLine());
-            int secondNumber = int.Parse(Console.ReadLine());
-            int thirdNumber = int.Parse(Console.ReadLine());
-            int fourthNumber = int.Parse(Console.ReadLine());
+            var firstNumber = int.Parse(Console.ReadLine());
+            var secondNumber = int.Parse(Console.ReadLine());
+            var thirdNumber = int.Parse(Console.ReadLine());
+            var fourthNumber = int.Parse(Console.ReadLine());
 
             //printing the numbers in 4-digit debit card format
             Console.WriteLine($"{ firstNumber:d4} {secondNumber:d4} {thirdNumber:d4} {fourthNumber:d4}");
