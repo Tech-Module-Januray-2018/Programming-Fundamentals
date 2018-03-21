b885264034cbf3e74264e33cedba6b93d3fccec7
mariaslytherin <nirvana4ever>
2018-03-21 17:45:37 +0200

    added documentation of solution of problem 1

--- Solution 01/Solution 01/Program.cs
+++ Solution 01/Solution 01/Program.cs
@@ -5,11 +5,14 @@ namespace Solution_01
     class Program
     {
         static void Main(string[] args)
-        {
+        {   
+            //initializing four integers 
             int firstNumber = int.Parse(Console.ReadLine());
             int secondNumber = int.Parse(Console.ReadLine());
             int thirdNumber = int.Parse(Console.ReadLine());
             int fourthNumber = int.Parse(Console.ReadLine());
+
+            //printing the numbers in 4-digit debit card format
             Console.WriteLine($"{ firstNumber:d4} {secondNumber:d4} {thirdNumber:d4} {fourthNumber:d4}");
         }
     }
